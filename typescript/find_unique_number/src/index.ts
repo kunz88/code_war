b37b54d4cb668ffbae163c55bb0c43951c457ae6
kunz88 <cunsolostefano@yahoo.it>
2023-12-16 12:20:16 +0100

    adding find unique exercise

---
+++ typescript/find_unique_number/src/index.ts
@@ -0,0 +1,18 @@
+/* There is an array with some numbers. All numbers are equal except for one. Try to find it!
+
+findUniq([ 1, 1, 1, 2, 1, 1 ]) === 2
+findUniq([ 0, 0, 0.55, 0, 0 ]) === 0.55
+It’s guaranteed that array contains at least 3 numbers.
+
+The tests contain some very huge arrays, so think about performance. */
+
+const findUniq = (arr: number[]): number|undefined => {
+    const firstElement = arr[0]
+    arr.shift()
+    if(arr.some((item)=> item === firstElement)){
+        return arr.find(item => item !== firstElement)
+    }else{
+        return firstElement
+    }
+  }
+  console.log(findUniq([ 0, 0, 0.55, 0, 0 ]))
